a8d638f54f326b2226039701c6728b81159e8af4
AnnaVitry <anne-marion119@hotmail.com>
2026-03-05 22:59:28 +0400

    main.py pour app api et front ok, gitignore ajouté, .env ajouté, uvicorn ok, streamlit ok

--- app_front/main.py
+++ app_front/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from app-front!")
+import os
 
+import streamlit as st
+from dotenv import load_dotenv
 
-if __name__ == "__main__":
-    main()
+# Charge les variables du fichier .env
+load_dotenv()
+
+# Récupère l'URL ou utilise une valeur par défaut
+API_URL = os.getenv("API_URL", "http://localhost:8000")
+
+st.title("Toolbox IA")
+# Utilise ensuite la variable API_URL dans tes requêtes
+# response = requests.get(f"{API_URL}/compute/add?a={a}&b={b}")
